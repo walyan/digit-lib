ffadd8a7c81adbc7d642eaea1dcec62067496aa1
walyan <walyanmuhamad5@gmail.com>
2025-03-14 13:19:30 +0700

    category

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('categories', CategoryController::class);
